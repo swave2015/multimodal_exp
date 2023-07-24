f730335cb0af977787db58cf4486fe263f9d8dff
caoxh <caoxiaohang@yeah.net>
2023-07-24 02:55:06 +0000

    modify folder path

--- demo_module/test.py
+++ demo_module/test.py
@@ -1,1 +1,9 @@
-from YOLOv8_TensorRT.models.engine import TRTModule
+import sys
+sys.path.insert(0, '../../multimodal_exp')
+from lavis.models import load_model_and_preprocess
+import torch
+
+device = "cuda" if torch.cuda.is_available() else "cpu"
+
+if __name__ == '__main__':
+    model, vis_processors, _ = load_model_and_preprocess(name="blip2_opt", model_type="pretrain_opt6.7b", is_eval=True, device=device)
